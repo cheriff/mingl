8de15675ce1456d643c2610948d9d8be7dc9e846
David <david.mirabito@gmail.com>
2016-04-28 19:47:07 +1000

    3rd time the charm: get makefile deps correct

--- external_code.mk
+++ external_code.mk
@@ -1,14 +1,21 @@
-EXTERN=glfw glm imgui
-.PHONY: $(EXTERN)
+EXTERN=$(GLFW_DEP) $(GLM_DEP) $(IMGUI_DEP)
+
 GOOD_GLFW_REV=d97044d9acc0c76433edda5a75b3196957906ca5
 GOOD_GLM_REV=79643cf4fa210ed8a5509243ef4613b555a9441c
 GOOD_IMGUI_REV=c5149cd53c80d022e6924b20e964544e9537cb20
+
+extern/lib extern/include:
+	@mkdir -p extern/lib
+	@mkdir -p extern/include
 
 
 GLFW_SRC=extern/code/glfw-src/README.md
 GLFW_OBJDIR=extern/code/glfw-obj/Makefile
 BUILD_GLFW=extern/code/glfw-obj/src/libglfw3.a
 LIB_GLFW=extern/lib/libglfw3.a
+INC_GLFW=extern/include/GLFW/glfw3.h
+GLFW_DEP=$(LIB_GLFW) $(INC_GLFW)
+
 
 .PHONY: glfw
 glfw: extern/include/GLFW extern/lib/libglfw3.a
@@ -27,27 +34,23 @@ $(GLFW_OBJDIR): $(GLFW_SRC)
 $(BUILD_GLFW): $(GLFW_OBJDIR)
 	$(MAKE) -C extern/code/glfw-obj glfw
 
-
-extern/include/GLFW: $(GLFW_SRC) | extern/include
+$(INC_GLFW): $(GLFW_SRC) | extern/include
 	cp -R ./extern/code/glfw-src/include/GLFW extern/include/GLFW
 
 $(LIB_GLFW): $(BUILD_GLFW)  | extern/lib
 	cp $< $@
 
-extern/lib extern/include:
-	@mkdir -p extern/lib
-	@mkdir -p extern/include
-
 
 
 glm: extern/include/glm
 GLM_SRC=extern/code/glm-src/readme.md
+GLM_DEP=extern/include/glm
 
 $(GLM_SRC):
 	git clone https://github.com/g-truc/glm.git extern/code/glm-src
 	(cd extern/code/glm-src && git reset --hard $(GOOD_GLM_REV))
 
-extern/include/glm: $(GLM_SRC) | extern/include
+$(GLM_DEP): $(GLM_SRC) | extern/include
 	mkdir -p extern/include
 	cp -R extern/code/glm-src/glm extern/include
 
@@ -56,8 +59,6 @@ LDFLAGS+=-Lextern/lib -lglfw3 \
 	-framework Cocoa -framework OpenGL -framework IOKit \
 	-framework CoreVideo  -framework Carbon
 
-
-imgui: extern/include/imgui.h extern/include/imconfig.h extern/lib/libimgui.a
 
 IMGUI_SRC=extern/code/imgui-src/README.md
 
@@ -69,13 +70,14 @@ extern/code/imgui-src/imgui_draw.cpp: $(IMGUI_SRC)
 extern/include/%.h: extern/code/imgui-src/%.h
 	cp $< $@
 
-
 IMGUI_OBJ=extern/code/imgui-src/imgui.o extern/code/imgui-src/imgui_draw.o
 $(IMGUI_OBJ): extern/code/imgui-src/%.o: extern/code/imgui-src/%.cpp
 	$(CC)  $(CFLAGS) -c $^ -o $@
 
 extern/lib/libimgui.a: $(IMGUI_OBJ)
 	$(AR) cr $@ $(IMGUI_OBJ)
+
+IMGUI_DEP=extern/lib/libimgui.a extern/include/imgui.h extern/include/imconfig.h
 
 
 $(IMGUI_SRC):
